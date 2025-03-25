1fb409348bc185f32388ff4cd86852896afb9b3a
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-03-25 11:28:15 +0530

    kbnk

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,7 +6,6 @@ import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-
     plugins: [
         vue(),
         vueDevTools(),
@@ -22,22 +21,5 @@ export default defineConfig({
         }
     },
 
-    build: {
-        rollupOptions: {
-          output: {
-            manualChunks(id) {
-              if (id.includes('node_modules')) {
-                if (id.includes('vue')) {
-                  return 'vue'; // Separate Vue-related files
-                }
-                if (id.includes('lodash')) {
-                  return 'lodash'; // Separate Lodash
-                }
-                return 'vendor'; // All other libraries
-              }
-            },
-          },
-        },
-      },
-    
+   
 });
